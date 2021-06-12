148dbd403e38cf97f1604f25f9fb2dfb98d4d0e3
Doe Mohamud <doe.mahamud1@gmail.com>
2021-06-12 16:15:48 -0400

    commit tree 9d1bca730b9bdd3b1cda63af39c9c9166b1e648a

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,38 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+  def self.new_by_name (s_name)
+    song = Song.new
+    song.name = s_name 
+    song
+  end
+
+  def self.create_by_name(s_name)
+    song = Song.create
+    song.name = s_name
+    song
+  end
+
+  def self.find_by_name(s_name)
+    @@all.find {|song| song.name == s_name}
+  end
+
+  def self.find_or_create_by_name(s_name)
+    self.find_by_name(s_name) or self.create_by_name(s_name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by do |s|
+      s.name
+    end
+  end
+
+  def self.new_from_filename(filename)
+
+  end
 end
